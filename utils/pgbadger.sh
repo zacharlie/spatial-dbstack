44f7298722ed5c8722c6bf8c135922192b1d4db9
Charlie <64078329+zacharlie@users.noreply.github.com>
2022-03-16 14:57:27 +0200

    pgbadger util

--- utils/pgbadger.sh
+++ utils/pgbadger.sh
@@ -1,1 +1,15 @@
-docker run --rm -v ./logs/pgbadger:/data -v ./logs/pg_log:/logs -e PGBADGER_DATA=/data uphold/pgbadger /logs/postgresql.log --jobs 4 --outdir /data postgresql-%Y-%m-%d_%H%M%S.log
+#! /usr/bin/env bash
+
+LOG_FILES="/logs/postgresql.log"
+
+# build list of available log files
+pushd "./logs/pg_log" || exit  # ensure operation is run from the target directory
+for file in *.{log,txt}; do \
+    LOG_FILES="${LOG_FILES} /logs/${file}"
+done
+popd || exit
+
+docker run --rm -v ./logs/pgbadger:/data \
+                -v ./logs/pg_log:/logs \
+                -e PGBADGER_DATA=/data \
+                uphold/pgbadger ${LOG_FILES} --jobs 2 --outdir /data --exclude-query "(pgadmin|)"
